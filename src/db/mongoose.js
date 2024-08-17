2ee6650ba3d94fdac017974cc6791b0ff6c244c3
Sean Conroy <seanconroy2001@gmail.com>
2024-08-17 13:21:51 +0100

    Implement docker

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-})
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
